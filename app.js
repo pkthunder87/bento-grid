18f00299bb8c5852bddda328030cdaf1b67fa8c4
pkthunder87 <pkthunder87@gmail.com>
2024-10-17 20:10:04 -0700

    Feat: Add responsive design for 2 grid items at 1440px

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ import Masonry from "masonry-layout";
 window.onload = () => {
   const grid = document.querySelector(".grid");
 
+  const gutterSize = window.innerWidth > 1400 ? 31.5 : 25;
+  console.log(gutterSize);
+
   const masonry = new Masonry(grid, {
     itemSelector: ".grid-item",
     columnWidth: ".grid-sizer",
     percentPosition: true,
     horizontalOrder: true,
-    gutter: 25,
+    gutter: gutterSize,
   });
 
   grid.imagesLoaded.progress(function () {
